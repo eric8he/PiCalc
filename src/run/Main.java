83148484e16a20da50b1da0b305fdb7a800f7630
s-hee <s-hee@51S500041918.it.bsd405.org>
2019-11-21 08:01:11 -0800

    Second commit

--- src/run/Main.java
+++ src/run/Main.java
@@ -5,57 +5,73 @@ import org.fusesource.jansi.*;
 
 import org.apfloat.*;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(calculateChudnovsky(100000).toString());
+		Scanner s = new Scanner(System.in);
+		
+		AnsiConsole.systemInstall();
+		while(true) {
+			System.out.println(new Ansi().fgBrightCyan().bgCyan().a("Digits to compute:"));
+			int p = Integer.parseInt(s.nextLine());
+			
+			System.out.println(calculateChudnovsky(p));
+		}
 	}
 	
 	public static Apfloat calculateChudnovsky(long precision) {
 		long k = 0;
 		long begin = System.currentTimeMillis();
 		
-		Apfloat a = new Apfloat(545140134,precision);
-		Apfloat b = new Apfloat("-262537412640768000",precision);
-		Apfloat c = new Apfloat(12, precision);
-		Apfloat d = new Apfloat(6, precision);
+		Apfloat a = new Apfloat(545140134,(int)(precision*1.2));
+		Apfloat b = new Apfloat("-262537412640768000",(int)(precision*1.2));
+		Apfloat c = new Apfloat(12,(int)(precision*1.2));
+		Apfloat d = new Apfloat(16,(int)(precision*1.2));
 		
-		Apfloat sum = new Apfloat(13591409,precision);
+		Apfloat sum = new Apfloat(0,(int)(precision*1.2));
 		
-		Apfloat C = new Apfloat(426880,precision).multiply(ApfloatMath.sqrt(new Apfloat(10005,precision)));
+		Apfloat C = new Apfloat(426880,(int)(precision*1.2)).multiply(ApfloatMath.sqrt(new Apfloat(10005,(int)(precision*1.2))));
 		
-		Apfloat L = new Apfloat(13591409,precision);
-		Apfloat X = new Apfloat(1,precision);
-		Apfloat M = new Apfloat(1,precision);
-		Apfloat K = new Apfloat(6,precision);
+		Apfloat L = new Apfloat(13591409,(int)(precision*1.2));
+		Apfloat X = new Apfloat(1,(int)(precision*1.2));
+		Apfloat M = new Apfloat(1,(int)(precision*1.2));
+		Apfloat K = new Apfloat(6,(int)(precision*1.2));
 		Apfloat H;
 		
-		Apfloat prev = new Apfloat(-1,precision);
 		
 		long end = System.currentTimeMillis();
 		
 		System.out.println(new Ansi().fgBrightRed().a("Initialization took "+(end-begin)+" ms."));
 		
 		begin = System.currentTimeMillis();
-		while(k<precision) {
-			long start = System.currentTimeMillis();
+		
+		System.out.print("Iteration: 0");
+		
+		while(k<Math.max(1L,(long) ((double)precision/32.654450041768516))) {
+			//long start = System.currentTimeMillis();
 			sum = sum.add(M.multiply(L).divide(X));
 			L = L.add(a);
 			X = X.multiply(b);
-			H = new Apfloat(k+1,precision);
+			H = new Apfloat(k+1,(int)(precision*1.2));
 			M = M.multiply(K.multiply(K.multiply(K)).subtract(d.multiply(K)).divide(H.multiply(H.multiply(H))));
 			K = K.add(c);
 			k++;
-			long finish = System.currentTimeMillis();
-			System.out.println("Iteration "+k+" finished in "+(finish-start)+"ms.");
+			if(k%100==0) System.out.print(new Ansi().cursorToColumn(0).a("Iteration: "+k));
+			//long finish = System.currentTimeMillis();
+			//System.out.println("Iteration "+k+" finished in "+(finish-start)+"ms.");
 		}
+		
+		System.out.println("\n");
+		
 		end = System.currentTimeMillis();
 		System.out.println(new Ansi().fgBrightGreen().a("Done. Summing series took "+(end-begin)+"ms."));
 		
 		System.out.println(new Ansi().fgBlue().a("Doing final division..."));
 		
 		begin = System.currentTimeMillis();
-		Apfloat pi = C.divide(sum).multiply(new Apfloat(2));
+		Apfloat pi = new Apfloat(C.divide(sum).toString().substring(0,(int)(precision+2)));
 		end = System.currentTimeMillis();
 		System.out.println(new Ansi().fgBrightGreen().a("Done. Division took "+(end-begin)+"ms."));
 		return pi;
